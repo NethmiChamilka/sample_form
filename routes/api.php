ea7b0f6a33d35e3d52f17472ab0e5638c33201ad
Nethmi Chamilka <154015199+NethmiChamilka@users.noreply.github.com>
2024-11-04 12:20:41 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-
-
 use App\Http\Controllers\ClientController;
 
 Route::post('/clients', [ClientController::class, 'store']);
